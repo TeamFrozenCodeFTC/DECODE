780cf77f90cdad192c22fcb3bbab16f3fe9cbb7a
Samuel <sfisher9793@gmail.com>
2025-11-08 11:24:36 -0600

    april tag

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/AprilTag.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/AprilTag.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.subsystems.Spindexer;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
@@ -42,6 +43,37 @@ public class AprilTag {
     public void stop() {
         visionPortal.stopStreaming();
         visionPortal.close();
+    }
+
+    public Spindexer.Artifact[] getPattern(){
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+
+        if(currentDetections.size()==0) {
+            return null;
+        }
+
+        AprilTagDetection detection= currentDetections.get(0);
+        if (detection.id==22){
+             return new Spindexer.Artifact[] {
+                 Spindexer.Artifact.PURPLE,
+                     Spindexer.Artifact.GREEN,
+                     Spindexer.Artifact.PURPLE
+             };
+        } else if (detection.id==23){
+            return new Spindexer.Artifact[] {
+                    Spindexer.Artifact.PURPLE,
+                    Spindexer.Artifact.PURPLE,
+                    Spindexer.Artifact.GREEN
+            };
+        } else if (detection.id ==21) {
+            return new Spindexer.Artifact[]{
+                    Spindexer.Artifact.GREEN,
+                    Spindexer.Artifact.PURPLE,
+                    Spindexer.Artifact.PURPLE
+            };
+        } else {
+            return null;
+        }
     }
 
     // get april tag information
