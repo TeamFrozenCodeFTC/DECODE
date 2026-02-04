63d5d2f10a20e7dec033e54ee8d7e13d665ad4f7
Samuel <sfisher9793@gmail.com>
2026-02-03 20:52:21 -0600

    limelight

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/MotifDetector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/MotifDetector.java
@@ -1,70 +1,60 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.Artifact;
-import org.firstinspires.ftc.vision.VisionPortal;
-import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
-import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
 import java.util.List;
 
 public class MotifDetector {
-    /**
-     * The variable to store our instance of the AprilTag processor.
-     */
-    private AprilTagProcessor aprilTag;
-    
-    /**
-     * The variable to store our instance of the vision portal.
-     */
-    private VisionPortal visionPortal;
-    
+    private Limelight3A limelight;
     private final HardwareMap hardwareMap;
-    
+
     public MotifDetector(HardwareMap map) {
         hardwareMap = map;
     }
-    
+
     public void start() {
-        aprilTag = AprilTagProcessor.easyCreateWithDefaults();
-        visionPortal = VisionPortal.easyCreateWithDefaults(
-            hardwareMap.get(WebcamName.class, "Webcam 1"), aprilTag);
+        limelight = hardwareMap.get(Limelight3A.class, "Limelight");
+        limelight.pipelineSwitch(2);
+        limelight.start();
     }
-    
+
     public void stop() {
-        visionPortal.stopStreaming();
-        visionPortal.close();
+        limelight.shutdown();
     }
-    
+
     public Artifact[] getMotifPattern() {
-        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
-        
-        if (currentDetections.isEmpty()) {
-            return null;
-        }
-        
-        for (AprilTagDetection detection : currentDetections) {
-            // all detections
-            if (detection.id == 22) {
-                return new Artifact[]{
-                    Artifact.PURPLE,
-                    Artifact.GREEN,
-                    Artifact.PURPLE
-                };
-            } else if (detection.id == 23) {
-                return new Artifact[]{
-                    Artifact.PURPLE,
-                    Artifact.PURPLE,
-                    Artifact.GREEN
-                };
-            } else if (detection.id == 21) {
-                return new Artifact[]{
-                    Artifact.GREEN,
-                    Artifact.PURPLE,
-                    Artifact.PURPLE
-                };
+        int tagid;
+        LLResult result = limelight.getLatestResult();
+        if (result.isValid()) {
+            // Access fiducial results
+            List<LLResultTypes.FiducialResult> fiducialResults = result.getFiducialResults();
+            for (LLResultTypes.FiducialResult fr : fiducialResults) {
+                tagid = fr.getFiducialId();
+                // all detections
+                if (tagid == 22) {
+                    return new Artifact[]{
+                            Artifact.PURPLE,
+                            Artifact.GREEN,
+                            Artifact.PURPLE
+                    };
+                } else if (tagid == 23) {
+                    return new Artifact[]{
+                            Artifact.PURPLE,
+                            Artifact.PURPLE,
+                            Artifact.GREEN
+                    };
+                } else if (tagid == 21) {
+                    return new Artifact[]{
+                            Artifact.GREEN,
+                            Artifact.PURPLE,
+                            Artifact.PURPLE
+                    };
+                }
             }
         }
         return null;
